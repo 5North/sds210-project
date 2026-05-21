d48ae0f0be6854ef5f76bcfb82dd4da03980c619
5North <199603038+5North@users.noreply.github.com>
2026-05-21 22:18:03 +0200

    Add function docstring

--- src/request_handler.py
+++ src/request_handler.py
@@ -4,6 +4,34 @@ import requests
 def handle_get_request(
     url: str, parameters: dict[str, str] | None = None
 ) -> requests.Response:
+    """
+    Handle responses status codes.
+
+    Handle the status codes GET requests's responses,
+    by returning the response if status code is OK and throwing
+    a base Exception with an error message in any other case.
+
+    Parameters
+    ----------
+    url : str
+        Url to send the GET request.
+    parameters : dict [str, str] | None
+        url parameters (default is None).
+
+    Returns
+    -------
+    request.Response
+        A request.Response object.
+
+    Examples
+    --------
+    url = "kittens-photos.com"
+    try:
+        response = handle_get_request(url)
+    except Exception as e:
+        print(f"An exception occured with msg: {e}")
+    photos = response.content
+    """
     response = requests.get(url, params=parameters)
     if response.status_code == 200:
         return response
